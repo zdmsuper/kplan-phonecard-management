7929f4477ccba9eb3198b914a6427302e0055185
zhangdeming <zhangdemin_51@163.com>
2020-12-07 10:16:31 +0800

    202012071016

--- src/main/java/com/kplan/phonecard/manager/KplanPhonenumBerManager.java
+++ src/main/java/com/kplan/phonecard/manager/KplanPhonenumBerManager.java
@@ -25,9 +25,9 @@ KplanPhoneNumberService kplanPhoneService;
 	public List<KplanPhoneNumber> findPhoneList(String phoneRule,String procductCode){
 		String sql;
 		if(StringUtils.trimToNull(phoneRule)==null) {
-			sql = "select phone,province_name,city_name,rule_name,last_date from kplan_phone_number where province_code='81' and city_code='810' and use_not=0 and rule_name!='尾号匹配'  and product_code='"+procductCode+"' order by random(),last_date desc limit 3";
+			sql = "select phone,province_name,city_name,rule_name,last_date from kplan_phone_number where province_code='81' and city_code='810' and use_not=0 and rule_name!='尾号匹配'  and product_code='"+procductCode+"' order by random(),last_date desc limit 2";
 		}else {
-			sql = "select phone,province_name,city_name,rule_name,last_date from kplan_phone_number where province_code='81' and city_code='810' and use_not=0  and rule_name='"+phoneRule+"' and product_code='"+procductCode+"' order by random(),last_date desc limit 3";
+			sql = "select phone,province_name,city_name,rule_name,last_date from kplan_phone_number where province_code='81' and city_code='810' and use_not=0  and rule_name='"+phoneRule+"' and product_code='"+procductCode+"' order by random(),last_date desc limit 2";
 		}
 	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	List<Object[]> result =this.kplanPhoneService.getNativeResultList(sql);
